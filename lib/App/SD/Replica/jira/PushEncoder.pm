651281863f8f4fd91620d65ca9cff45eaeba74cd
Nate Murray <nate@natemurray.com>
2009-09-22 11:58:55 -0700

    s/github/jira/g

--- lib/App/SD/Replica/jira/PushEncoder.pm
+++ lib/App/SD/Replica/jira/PushEncoder.pm
@@ -1,10 +1,10 @@
-package App::SD::Replica::github::PushEncoder;
+package App::SD::Replica::jira::PushEncoder;
 use Any::Moose;
 use Params::Validate;
 use Path::Class;
 
 has sync_source => (
-    isa => 'App::SD::Replica::github',
+    isa => 'App::SD::Replica::jira',
     is  => 'rw',
 );
 
@@ -78,7 +78,7 @@ sub integrate_ticket_update {
     # Figure out the remote site's ticket ID for this change's record
     my $remote_ticket_id =
       $self->sync_source->remote_id_for_uuid( $change->record_uuid );
-    my $ticket = $self->sync_source->github->issue();
+    my $ticket = $self->sync_source->jira->issue();
     my $attr = $self->_recode_props_for_integrate($change);
     $ticket->edit( $remote_ticket_id, $attr->{title}, $attr->{body} );
     if ( $attr->{status} ) {
@@ -97,7 +97,7 @@ sub integrate_ticket_create {
     );
 
     # Build up a ticket object out of all the record's attributes
-    my $ticket = $self->sync_source->github->issue;
+    my $ticket = $self->sync_source->jira->issue;
     my $attr = $self->_recode_props_for_integrate($change);
     my $new =
       $ticket->open( $attr->{title}, $attr->{body} );
@@ -120,7 +120,7 @@ sub integrate_comment {
 
     my %props = map { $_->name => $_->new_value } $change->prop_changes;
     my $ticket_id = $self->sync_source->remote_id_for_uuid( $props{'ticket'} );
-    my $ticket = $self->sync_source->github->issue();
+    my $ticket = $self->sync_source->jira->issue();
     $ticket->comment($ticket_id, $props{'content'});
     return $ticket_id;
 }
